59ff562b057ed5278fd02b417f6b7ab062f7b884
S.Sai Ram <162550829+Sai23ram@users.noreply.github.com>
2024-08-13 13:46:35 +0530

    Add files via upload

---
+++ Introduction to python.py
@@ -0,0 +1,27 @@
+#introduction to python
+'''what is Programming? --> Programming is a set of instructions that are given to a computer to execute something.
+what is Python? --> Python is a scripting,programming,general based,highlevel programming language.
+what is Scripting Language? --> Any programming language that is interpreted is called a Scripting Language.
+what is an interpreter? --> It converts the high level code to machine code(low level code) line by line.
+What is an Translator? --> Any device that may be a compiler , interpreter , assembler that converts high level code to low level code is called a translator.
+Is python Dynamic? --> Yes,Python is a dynamic programming language because we don't need to give datatype of a variable in python as it takes automatically.
+In python,DataTypes are classes and methods are objects.
+To know the datatype of a variable we need to use type(variable name).
+'''
+a=10#int
+b=20.2#float
+c=34.0987654321222333344455556667777888888#decimal
+d='s'#character
+e="ram"#string
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+#to interpret python file in command prompt we have to use py filename.py
+
